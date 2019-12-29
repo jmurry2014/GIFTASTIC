823cb5b7ab6fd7b33c405f96e28b9570365a9321
James Murry <jmurry2014@gmail.com>
2019-12-29 00:11:48 -0600

    finished the giphy api and succesfully added teh ability to add in new strigns into array

--- gif.js
+++ gif.js
@@ -6,6 +6,7 @@ $(function () {
 })
 
 function populateButtons(classToAdd, areaToAddTo, narutoArray) {
+    $("#searches").empty()
     for (var i = 0; i < narutoArray.length; i++) {
         var b = $("<button>");
         b.addClass(classToAdd);
@@ -14,3 +15,74 @@ function populateButtons(classToAdd, areaToAddTo, narutoArray) {
         $(areaToAddTo).append(b)
     }
 }
+
+
+
+$(document).on('click', '.narutoButton', function () {
+
+
+    var character = $(this).data('type')
+    console.log(character)
+    var queryURL = 'http://api.giphy.com/v1/gifs/search?q=' + character + '&api_key=tQNI7UCgtwJRKmpgwwySCiQr8nnzaA4u&limit=5'
+
+
+    $.ajax({
+        url: queryURL,
+        method: 'GET'
+    }).then(function (response) {
+
+        for (var i = 0; i < response.data.length; i++) {
+            var still = response.data[i].images.fixed_height_still.url
+            var animate = response.data[i].images.fixed_height.url
+            var rating = response.data[i].rating
+            var p = $("<p>").text("Rating: " + rating)
+            var image = $("<img>")
+            image.attr('src', still)
+            image.attr('data-state', 'still')
+            image.attr('data-animate', animate)
+            image.attr('data-still', still)
+            image.addClass('move')
+            // $("#pics").append(p)
+
+            $("#pics").append(image)
+
+        }
+    })
+
+})
+
+$(document).on('click', "#add-choice", function (event) {
+    event.preventDefault()
+    var newCharacter = $('input').eq(0).val();
+    narutoArray.push(newCharacter)
+    populateButtons('narutoButton', '#searches', narutoArray)
+
+    return false
+
+})
+
+
+
+
+
+
+$(document).on('click', '.move', function () {
+    var state = $(this).attr('data-state')
+
+    if (state === 'still') {
+        $(this).attr('src', $(this).attr('data-animate'))
+        $(this).attr('data-state', 'animated')
+    } else {
+        $(this).attr('src', $(this).attr('data-still'))
+        $(this).attr('data-state', 'still')
+    }
+
+})
+
+
+
+
+
+
+
+
